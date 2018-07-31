5a1437e2112dcba17a284ad3588f0a0a199dca59
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-31 17:03:10 -0500

    Modifica contenido desplegado en 'index.html'

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import './master.css';
+
+const elP = document .createElement( 'p' ),
+      $container = document .getElementById( 'container' );
+
+elP .textContent = 'Este es un ejemplo básico de WebPack';
+$container .appendChild( elP );
+
 console .log( 'Hola Mundo!' );
