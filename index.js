2ed58778a574f8b5ce23911c2440017f6977a0b0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-31 18:14:08 -0500

    Filtramos cambios para archivo específico usando 'HotModuleReplacementPlugin'

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import './master.css';
+import message from './message.js';
 
 const elP = document .createElement( 'p' ),
       $container = document .getElementById( 'container' );
 
-elP .textContent = 'Este es un ejemplo básico de WebPack';
+const printMessage = () => {
+    elP .textContent = `Este es un ejemplo básico de WebPack usando ${ message() }`;
+}
+printMessage();
+
 $container .appendChild( elP );
 
 console .log( 'Hola Mundo!' );
+
+/* Validamos si el módulo HotModuleReplacementPlugin esta activo 'caliente' */
+if( module .hot ) {
+    /* Aquí colocamos el archivo que queremos escuchar por cambios */
+    module .hot .accept(
+        './message.js',       /* Path: del archivo a escuchar */
+        () => { /* Acción que vamos a realizar */
+            console .log( 'Ha cambiado -> message.js' );
+            printMessage();
+        }
+    );
+}
---
+++ message.js
@@ -0,0 +1,7 @@
+function message() {
+    const text = '"Hot Module Replacement Plugin"';
+    console .log( 'message', text );
+    return text;
+}
+
+export default message;
